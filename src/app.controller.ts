bb0aefedfaaa4575611e3b6ba7aea4f1cc136764
TakashiAihara <takashi.aihara.0523@gmail.com>
2022-09-01 04:23:14 +0000

    first fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
-@Controller()
+@Controller('*')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  get(@Req() request: Request): string {
+    console.log(this.appService.get(request.url));
+    return this.appService.get(request.url);
+  }
+
+  @Post()
+  set(@Body() obj: object, @Req() request: Request): void {
+    console.log(JSON.stringify(obj));
+    console.log(request.url);
+    return this.appService.set(request.url, obj);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  get(url: string): string {
+    if (url.includes('.')) {
+      return '';
+    }
+    const mockPath = this.parsePath(url);
+    return fs.readFileSync(mockPath, 'utf-8');
+  }
+
+  set(url: string, obj: object): void {
+    const mockPath = this.parsePath(url);
+    console.log();
+    if (!fs.existsSync(path.dirname(mockPath))) {
+      fs.mkdirSync(path.dirname(mockPath));
+    }
+    fs.writeFileSync(mockPath, JSON.stringify(obj, null, ' '));
+  }
+
+  parsePath(url: string): string {
+    const parsedUrl = url.replace('?', '--');
+    const mockDir = path.resolve(__dirname, '../mock');
+    const mockPath = path.join(mockDir, parsedUrl) + '.json';
+    console.log(mockPath);
+    return mockPath;
   }
 }
